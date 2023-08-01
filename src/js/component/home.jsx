d029458e9295746f11d5f3b08f933f8ae2389f56
jose carlos perdomo herrera <phpperdomo@gmail.com>
2023-07-31 21:24:09 -0400

    creando componente counter

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,5 +1,6 @@
-import { useState } from "react";
+
 import React  from "react";
+import Counter from "./secondsCounter";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
@@ -9,18 +10,7 @@ const Home = (props) => {
 
 	return (
 		<>
-		<div className="container bg-dark mt-5 d-flex  align-items-center justify-content-center">
-				<div className=" row ">
-					<div className="card col-2 fs-1  text-bg-dark p-3 fw-bolder d-flex  align-items-center justify-content-center ">
-						<i class="far fa-clock"></i>
-					</div>
-					<div className="card col-2 fs-1  text-bg-dark p-3 fw-bolder">{props.num5}</div>
-					<div className="card col-2 fs-1 text-bg-dark p-3 fw-bolder">{props.num4}</div>
-					<div className="card col-2 fs-1 text-bg-dark p-3 fw-bolder">{props.num3}</div>
-					<div className="card col-2 fs-1 text-bg-dark p-3 fw-bolder">{props.num2}</div>
-					<div className="card col-2 fs-1 text-bg-dark p-3 fw-bolder">{props.num1}</div>
-				</div>
-		</div>
+		<Counter num1={props.num1}  num2={props.num2}  num3={props.num3}  num4={props.num4}  num5={props.num5}/>
 		</>
 	);
 };
