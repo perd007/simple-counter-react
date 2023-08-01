8fd7f31d52ff63ca6403950a32a4fae55c74eb27
jose carlos perdomo herrera <phpperdomo@gmail.com>
2023-07-31 20:06:46 -0400

    agregando reloj

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -8,9 +8,12 @@ import rigoImage from "../../img/rigo-baby.jpg";
 const Home = (props) => {
 
 	return (
+		<>
 		<div className="container bg-dark mt-5 d-flex  align-items-center justify-content-center">
 				<div className=" row ">
-				<div className="card col-2 fs-1  text-bg-dark p-3 fw-bolder"><i className="fa-regular fa-clock"></i></div>
+					<div className="card col-2 fs-1  text-bg-dark p-3 fw-bolder d-flex  align-items-center justify-content-center ">
+						<i class="far fa-clock"></i>
+					</div>
 					<div className="card col-2 fs-1  text-bg-dark p-3 fw-bolder">{props.num5}</div>
 					<div className="card col-2 fs-1 text-bg-dark p-3 fw-bolder">{props.num4}</div>
 					<div className="card col-2 fs-1 text-bg-dark p-3 fw-bolder">{props.num3}</div>
@@ -18,6 +21,7 @@ const Home = (props) => {
 					<div className="card col-2 fs-1 text-bg-dark p-3 fw-bolder">{props.num1}</div>
 				</div>
 		</div>
+		</>
 	);
 };
 
